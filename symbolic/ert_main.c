179d5cdc4ee761d048c26621becc5f65a9a4d4a4
Jason Biatek <biatek@cs.umn.edu>
2014-01-28 10:07:44 -0800

    Made number of steps to run an argument

--- symbolic/ert_main.c
+++ symbolic/ert_main.c
@@ -16,49 +16,43 @@
  */
 
 #include <stdio.h>                     /* This ert_main.c example uses printf/fflush */
+#include <stdlib.h>
+#include <errno.h>
 #include "microwave.h"                  /* Model's header file */
 #include "rtwtypes.h"                  /* MathWorks types */
 #include "klee_util.h"
 
-#define NUM_STEPS 3
 
-int step_num;
-ExtU_microwave_T inputs[NUM_STEPS];
 
-void make_input_symbolic(void);
-void make_input_symbolic(void) 
+void make_input_symbolic(ExtU_microwave_T *input);
+void make_input_symbolic(ExtU_microwave_T *input)
 {
-  int i = 0;
-  for (i = 0; i < NUM_STEPS; i++) {
-    klee_make_symbolic_range( &(inputs[i].KP_START), 0, 
-                        sizeof(boolean_T), "KP_START");
-    klee_make_symbolic_range( &(inputs[i].KP_CLEAR), 0, 
-                        sizeof(boolean_T), "KP_CLEAR");
-    klee_make_symbolic_range( &(inputs[i].KP_0), 0, 
-                        sizeof(boolean_T), "KP_0");
-    klee_make_symbolic_range( &(inputs[i].KP_1), 0, 
-                        sizeof(boolean_T), "KP_1");
-    klee_make_symbolic_range( &(inputs[i].KP_2), 0, 
-                        sizeof(boolean_T), "KP_2");
-    klee_make_symbolic_range( &(inputs[i].KP_3), 0, 
-                        sizeof(boolean_T), "KP_3");
-    klee_make_symbolic_range( &(inputs[i].KP_4), 0, 
-                        sizeof(boolean_T), "KP_4");
-    klee_make_symbolic_range( &(inputs[i].KP_5), 0, 
-                        sizeof(boolean_T), "KP_5");
-    klee_make_symbolic_range( &(inputs[i].KP_6), 0, 
-                        sizeof(boolean_T), "KP_6");
-    klee_make_symbolic_range( &(inputs[i].KP_7), 0, 
-                        sizeof(boolean_T), "KP_7");
-    klee_make_symbolic_range( &(inputs[i].KP_8), 0, 
-                        sizeof(boolean_T), "KP_8");
-    klee_make_symbolic_range( &(inputs[i].KP_9), 0, 
-                        sizeof(boolean_T), "KP_9");
-    klee_make_symbolic_range( &(inputs[i].DOOR_CLOSED), 0, 
-                        sizeof(boolean_T), "DOOR_CLOSED");
-  }
-
-  //klee_make_symbolic_range( &(inputs), 0, sizeof(inputs), "everything");
+  klee_make_symbolic_range( &(input->KP_START), 0, 
+                      sizeof(boolean_T), "KP_START");
+  klee_make_symbolic_range( &(input->KP_CLEAR), 0, 
+                      sizeof(boolean_T), "KP_CLEAR");
+  klee_make_symbolic_range( &(input->KP_0), 0, 
+                      sizeof(boolean_T), "KP_0");
+  klee_make_symbolic_range( &(input->KP_1), 0, 
+                      sizeof(boolean_T), "KP_1");
+  klee_make_symbolic_range( &(input->KP_2), 0, 
+                      sizeof(boolean_T), "KP_2");
+  klee_make_symbolic_range( &(input->KP_3), 0, 
+                      sizeof(boolean_T), "KP_3");
+  klee_make_symbolic_range( &(input->KP_4), 0, 
+                      sizeof(boolean_T), "KP_4");
+  klee_make_symbolic_range( &(input->KP_5), 0, 
+                      sizeof(boolean_T), "KP_5");
+  klee_make_symbolic_range( &(input->KP_6), 0, 
+                      sizeof(boolean_T), "KP_6");
+  klee_make_symbolic_range( &(input->KP_7), 0, 
+                      sizeof(boolean_T), "KP_7");
+  klee_make_symbolic_range( &(input->KP_8), 0, 
+                      sizeof(boolean_T), "KP_8");
+  klee_make_symbolic_range( &(input->KP_9), 0, 
+                      sizeof(boolean_T), "KP_9");
+  klee_make_symbolic_range( &(input->DOOR_CLOSED), 0, 
+                      sizeof(boolean_T), "DOOR_CLOSED");
 }
 
 /*
@@ -72,8 +66,8 @@ void make_input_symbolic(void)
  * your application needs.  This example simply sets an error status in the
  * real-time model and returns from rt_OneStep.
  */
-void rt_OneStep(void);
-void rt_OneStep(void)
+void rt_OneStep(ExtU_microwave_T *input);
+void rt_OneStep(ExtU_microwave_T *input)
 {
   static boolean_T OverrunFlag = 0;
 
@@ -90,7 +84,7 @@ void rt_OneStep(void)
   /* Save FPU context here (if necessary) */
   /* Re-enable timer or interrupt here */
   /* Set model inputs here */
-  microwave_U = inputs[step_num];
+  microwave_U = *input;
 
   /* Step the model */
   microwave_step();
@@ -114,11 +108,30 @@ void rt_OneStep(void)
 int_T main(int_T argc, const char_T *argv[]);
 int_T main(int_T argc, const char_T *argv[])
 {
+  /* How many steps to run? */
+  if (argc != 2) 
+  {
+    printf("Usage: microwave_type [STEPS]\n");
+  }
+  int num_steps = strtol(argv[1], NULL, 0);
+  if (errno || num_steps == 0)
+  {
+    perror("Error parsing num steps.\n");
+    exit(1);
+  }
+
   /* Initialize model */
   microwave_initialize(); // used to have an argument: 1.
 
   /* Create symbolic inputs beforehand */
-  make_input_symbolic();
+  //make_input_symbolic();
+  int step_num;
+  ExtU_microwave_T inputs[num_steps];
+  int i;
+  for (i=0; i<num_steps; i++)
+  {
+    make_input_symbolic( &(inputs[i]));
+  }
 
   /* Attach rt_OneStep to a timer or interrupt service routine with
    * period 1.0 seconds (the model's base sample time) here.  The
@@ -127,9 +140,9 @@ int_T main(int_T argc, const char_T *argv[])
    *  rt_OneStep();
    */
   step_num = 0;
-  while (rtmGetErrorStatus(microwave_M) == (NULL) && step_num < NUM_STEPS) {
+  while (rtmGetErrorStatus(microwave_M) == (NULL) && step_num < num_steps) {
     /*  Perform other application tasks here */
-    rt_OneStep();
+    rt_OneStep( &(inputs[step_num]));
     step_num++;
   }
 
